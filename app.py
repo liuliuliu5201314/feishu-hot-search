f6ff8c08d102b22ea4daec0e585dd2ee4faa35b4
w我就是我你是谁 <15801082933@163.com>
2026-06-16 02:13:14 +0800

    Fix field name to 视频标题

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def write_to_base(token, bvid, title, subtitle, author="", cover="", desc=""):
     
     data = {
         "fields": {
-            "标题": title,
+            "视频标题": title,
             "BV号": bvid,
             "字幕内容": subtitle[:2000],
             "抓取时间": timestamp_ms,
